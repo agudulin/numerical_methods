3289a98725f0646f9e0b57cf33b351953af67c53
Alexander Gudulin <alexandr.gudulin@gmail.com>
2014-09-07 17:06:36 +0400

    Added utils module and main module

---
+++ app/main.js
@@ -0,0 +1,5 @@
+var task1 = require("./tasks/1");
+var task2 = require("./tasks/2");
+
+// task1.solve();
+task2.solve();
---
+++ app/utils.js
@@ -0,0 +1,33 @@
+var math = require("mathjs");
+
+function clone(a) {
+  return JSON.parse(JSON.stringify(a));
+}
+
+function print(value) {
+  var precision = 14;
+  console.log(math.format(value, precision));
+}
+
+function printMatrix(matrix, precision) {
+  if (precision === undefined) {
+     precision = 4;
+  }
+  var matrixSize = matrix.length;
+  matrix.forEach(function (value, index) {
+    var line = "";
+    if (typeof value == "object") {
+      value.forEach(function (val, idx) {
+        line += math.format(val, precision) + "\t"
+      });
+      console.log(line);
+    } else {
+      console.log(math.format(value, precision));
+    }
+  });
+  console.log("");
+}
+
+exports.clone = clone;
+exports.print = print;
+exports.printMatrix = printMatrix;
